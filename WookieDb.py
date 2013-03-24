f534419dee8989b9d0c0f6f3bee62f9ffe886260
Christopher Hewett <chewett@hotmail.co.uk>
2013-03-24 23:00:57 +0000

    Named the class properly

--- WookieDb.py
+++ WookieDb.py
@@ -1,6 +1,6 @@
 import MySQLdb
 
-class wookieDb:
+class WookieDb:
 
     def __init__(self, host, user, password, db):
         self.connection = MySQLdb.Connect(host=host, user=user, passwd=password, db=db)
