582f47b3c360281a928b935f90ef00f8c238adac
Christopher Hewett <chewett@hotmail.co.uk>
2013-03-24 22:57:37 +0000

    Initial commit

---
+++ WookieDb.py
@@ -0,0 +1,8 @@
+import MySQLdb
+
+class wookieDb:
+
+    def __init__(self, host, user, password, db):
+        self.connection = MySQLdb.Connect(host=host, user=user, passwd=password, db=db)
+        self.cursor = self.connection.cursor()
+
