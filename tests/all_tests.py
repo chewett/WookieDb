da12e295f9e702b3ee04420c067c50e01886a179
Christopher Hewett <chewett@hotmail.co.uk>
2013-03-27 23:04:28 +0000

    Added lots more tests, does not fully work yet, freezes atm

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -30,6 +30,8 @@ class TestBasicCommands(unittest.TestCase):
         tables = self.db.query("show tables")
         self.assertFalse(("basic_test",) in tables)
 
+        self.db.query("DROP TABLE IF EXISTS basic_test")
+
         self.db.query("""CREATE TABLE `basic_test` (
                         `id` int(11) NOT NULL,
                         `intVar` int(11) DEFAULT NULL,
@@ -39,6 +41,62 @@ class TestBasicCommands(unittest.TestCase):
 
         tables = self.db.query("show tables")
         self.assertTrue(("basic_test",) in tables)
+
+
+
+
+class TestInsertStatements(unittest.TestCase):
+
+    def setUp(self):
+        self.db = WookieDb("localhost", "wookiedbtest", "wookiedbtest", "wookiedbtest")
+        self.db.query("DROP TABLE IF EXISTS `basic_test`")
+        self.db.query("""CREATE TABLE `basic_test` (
+                        `id` int(11) NOT NULL,
+                        `intVar` int(11) DEFAULT NULL,
+                        `charVar` varchar(45) DEFAULT NULL,
+                         PRIMARY KEY (`id`)
+                       ) ENGINE=InnoDB DEFAULT CHARSET=latin1""")
+        self.data = {"intVar": "123", "charVar": "testing"}
+
+    def testOneInsert(self):
+        res = self.db.query("select * from basic_test")
+        self.assertEqual(len(res), 0)
+        self.db.insert("test_basic", self.data)
+
+        res = self.db.query("select * from basic_test")
+        self.assertEqual(len(res), 1)
+
+
+
+class TestSelectStatements(unittest.TestCase):
+
+    def setUp(self):
+        self.db = WookieDb("localhost", "wookiedbtest", "wookiedbtest", "wookiedbtest")
+        self.db.query("DROP TABLE IF EXISTS `basic_test`")
+        self.db.query("""CREATE TABLE `basic_test` (
+                        `id` int(11) NOT NULL,
+                        `intVar` int(11) DEFAULT NULL,
+                        `charVar` varchar(45) DEFAULT NULL,
+                         PRIMARY KEY (`id`)
+                       ) ENGINE=InnoDB DEFAULT CHARSET=latin1""")
+        self.data = {"intVar": "123", "charVar": "testing"}
+        self.db.insert("basic_test", self.data)
+        self.db.insert("basic_test", self.data)
+        self.db.insert("basic_test", self.data)
+
+    def testBasicSelect(self):
+        res = self.db.select("basic_test", "*")
+        self.assertEqual(len(res), 3)
+        for r in res:
+            self.assertEqual(len(r), 3)
+
+        self.db.insert("basic_test", self.data)
+
+        res = self.db.select("basic_test", "*")
+        self.assertEqual(len(res), 4)
+        for r in res:
+            self.assertEqual(len(r), 3)
+
 
 if __name__ == "__main__":
     unittest.main()
