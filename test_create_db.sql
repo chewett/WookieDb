39da7c2dd69b98d1a34da4ab76d8008e2e7e4a3c
Christopher Hewett <chewett@hotmail.co.uk>
2013-03-27 20:49:09 +0000

    Added code to create a test table

---
+++ test_create_db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `basic_test` (
+    `id` int(11) NOT NULL,
+    `intVar` int(11) DEFAULT NULL,
+    `charVar` varchar(45) DEFAULT NULL,
+    PRiIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
