d1af510f71043323c085a3e9fb9b55b7c317169d
Christopher Hewett <chewett@hotmail.co.uk>
2013-03-27 22:28:01 +0000

    Fixed minor spelling issue

--- tests/test_create_db.sql
+++ tests/test_create_db.sql
@@ -2,6 +2,6 @@ CREATE TABLE `basic_test` (
     `id` int(11) NOT NULL,
     `intVar` int(11) DEFAULT NULL,
     `charVar` varchar(45) DEFAULT NULL,
-    PRiIMARY KEY (`id`)
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
 
