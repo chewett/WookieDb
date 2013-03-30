6db6d8fb1c9612ea17cc59c7e43038f2884f721a
Christopher Hewett <chewett@hotmail.co.uk>
2013-03-30 15:45:39 +0000

    Fixed concatanating none and return the fetchall from select

--- WookieDb.py
+++ WookieDb.py
@@ -6,13 +6,13 @@ class WookieDb:
         self.connection = MySQLdb.Connect(host=host, user=user, passwd=password, db=db)
         self.cursor = self.connection.cursor()
 
-    def select(self, table, fields, condition=None):
-        self.query("SELECT " + fields + " FROM " + table + condition)
+    def select(self, table, fields, condition=""):
+        return self.query("SELECT " + fields + " FROM " + table + condition)
 
     def insert(self, table, data):
         raise NotImplementedError("Not yet implemented, planned for future")
 
-    def update(self, table, data, condition=None):
+    def update(self, table, data, condition=""):
         raise NotImplementedError("Not yet implemented, planned for future")
 
     def query(self, sql):
