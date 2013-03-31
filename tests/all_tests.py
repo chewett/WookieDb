8ffcf9a3295627eebc0e71a514ddf25a549d4766
Christopher Hewett <chewett@hotmail.co.uk>
2013-03-31 23:20:32 +0100

    Removed the select test from that class

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -55,37 +55,5 @@ class TestInsertStatements(unittest.TestCase):
         res = self.db.query("select * from basic_test")
         self.assertEqual(len(res), 1)
 
-
-
-class TestSelectStatements(unittest.TestCase):
-
-    def setUp(self):
-        self.db = WookieDb("localhost", "wookiedbtest", "wookiedbtest", "wookiedbtest")
-        self.db.query("""CREATE TABLE `basic_test` (
-                        `id` int(11) NOT NULL,
-                        `intVar` int(11) DEFAULT NULL,
-                        `charVar` varchar(45) DEFAULT NULL,
-                         PRIMARY KEY (`id`)
-                       ) ENGINE=InnoDB DEFAULT CHARSET=latin1""")
-        self.data = {"intVar": "123", "charVar": "testing"}
-        self.db.query("INSERT INTO basic_test VALUES (,123, 'testing');")
-
-    def tearDown(self):
-        self.db.query("DROP TABLE basic_test")
-
-    def testBasicSelect(self):
-        res = self.db.select("basic_test", "*")
-        self.assertEqual(len(res), 3)
-        for r in res:
-            self.assertEqual(len(r), 3)
-
-        self.db.query("INSERT INTO basic_test VALUES ('',123, 'testing');")
-
-        res = self.db.select("basic_test", "*")
-        self.assertEqual(len(res), 4)
-        for r in res:
-            self.assertEqual(len(r), 3)
-
-
 if __name__ == "__main__":
     unittest.main()
